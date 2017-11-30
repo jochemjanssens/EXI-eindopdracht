db275454b45e6ea1a3005594609e7fb4355d01cf
jochemjanssens <janssens.jochem@gmail.com>
2017-11-30 16:27:35 +0100

    #4 er zijn 4 lasers die werken

--- app.js
+++ app.js
@@ -43,33 +43,73 @@ board.on(`ready`, function() {
     pin: `A5`,
     freq: 10
   });
-  const led = new five.Led(11);
+  const photoresistor3 = new five.Sensor({
+    pin: `A2`,
+    freq: 10
+  });
+  const photoresistor4 = new five.Sensor({
+    pin: `A4`,
+    freq: 10
+  });
+  const laser1 = new five.Led(9);
+
+  laser1.on();
+
 
   let raak1 = false;
-  let raak2 = false;
-  led.fadeIn();
   photoresistor1.on(`data`, function() {
-    console.log('1: ' + this.value);
-    if (this.value > 950 && raak1 === false) {
+    console.log("1: " + this.value);
+    if (this.value > 900 && raak1 === false) {
       console.log(`raak1`);
       io.sockets.emit(`update`, {"one": true});
       raak1 = true;
-    } else if(this.value < 950 && raak1 === true)  {
+    } else if(this.value < 900 && raak1 === true)  {
       io.sockets.emit(`update`, {"one": false});
 
       raak1 = false;
     }
   });
+
+  let raak2 = false;
   photoresistor2.on(`data`, function() {
-    console.log('2: ' + this.value);
-    if (this.value > 950 && raak2 === false) {
-      console.log(`raak1`);
-      io.sockets.emit(`update`, {"two": true});
+
+      console.log("2: " + this.value);
+    if (this.value > 900 && raak2 === false) {
+      console.log(`raak2`);
+      io.sockets.emit(`update`, {"one": true});
       raak2 = true;
-    } else if(this.value < 950 && raak2 === true)  {
-      io.sockets.emit(`update`, {"two": false});
+    } else if(this.value < 900 && raak2 === true)  {
+      io.sockets.emit(`update`, {"one": false});
 
       raak2 = false;
+    }
+  });
+  let raak3 = false;
+  photoresistor3.on(`data`, function() {
+
+      console.log("3: " + this.value);
+    if (this.value > 900 && raak3 === false) {
+      console.log(`raak3`);
+      io.sockets.emit(`update`, {"one": true});
+      raak3 = true;
+    } else if(this.value < 900 && raak3 === true)  {
+      io.sockets.emit(`update`, {"one": false});
+
+      raak3 = false;
+    }
+  });
+  let raak4 = false;
+  photoresistor4.on(`data`, function() {
+
+      console.log("4: " + this.value);
+    if (this.value > 900 && raak4 === false) {
+      console.log(`raak4`);
+      io.sockets.emit(`update`, {"one": true});
+      raak4 = true;
+    } else if(this.value < 900 && raak4 === true)  {
+      io.sockets.emit(`update`, {"one": false});
+
+      raak4 = false;
     }
   });
 });
